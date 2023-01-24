07cd9689d3ff8490b07b21a361dbc24de4fb2b17
Asadur <32617400+AsadurS@users.noreply.github.com>
2023-01-24 11:09:50 +0600

    add test action

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_get_response()
+    {
+        $response = $this->get('/d');
+
+        $response->assertStatus(200);
+    }
+}
